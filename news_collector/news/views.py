13fc3e7d4152701df5c0fc796e4d08f755522618
Sbonelo01 <sbonelomkhize15@gmail.com>
2020-05-06 09:39:00 +0200

    added news app

---
+++ news_collector/news/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+import requests
+from django.shortcuts import render, redirect
+from bs4 import BeautifulSoup as BSoup
+from news.model
